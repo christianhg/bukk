ba5c8aaed89a652a666b8b4c95280d0279952057
Christian Grøngaard <christianhg@gmail.com>
2016-12-31 12:07:21 +0100

    Fix typo

--- src/compose.js
+++ src/compose.js
@@ -2,6 +2,6 @@ const _pipe = require('./_pipe')
 const head = require('./head')
 const tail = require('./tail')
 
-module.exports = comose = (...fs) =>
+module.exports = compose = (...fs) =>
   (...args) =>
     _pipe(tail(fs.reverse()), head(fs.reverse()).apply(undefined, args))
