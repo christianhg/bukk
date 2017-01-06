5e09122ad5a9283e22397e01d5596fa210a6b079
Christian Grøngaard <christianhg@gmail.com>
2017-01-06 23:04:16 +0100

    B.any should also handle Maps, Objects, Sets and Strings

--- src/any.js
+++ src/any.js
@@ -1,6 +1,18 @@
 const curry = require('./curry')
+const type = require('./type')
 
-const any = (p, xs) =>
-  xs.some(p)
+const any = (p, a) =>
+  ({
+    'Array': () => a.some(p),
+    'Boolean': () => false,
+    'Map': () => Array.from(a).some(([key, value]) => p(value)),
+    'Null': () => false,
+    'Number': () => false,
+    'Object': () => Object.keys(a).some((x) => p(a[x])),
+    'Set': () => [...a].some(p),
+    'String': () => a.split('').some(c => p(c)),
+    'Symbol': () => false,
+    'Undefined': () => false
+  }[type(a)]())
 
 module.exports = curry(any)
--- tests/any.js
+++ tests/any.js
@@ -3,17 +3,45 @@ import test from 'ava'
 import any from '../src/any'
 import equals from '../src/equals'
 
-test('should return true if any elements match predicate', t => {
-  const isFoo = equals('foo')
+const isA = equals('a')
+const isFoo = equals('foo')
+const isZero = equals(0)
 
+test('should test predicate on Array', t => {
   t.is(any(isFoo, ['foo', 'foo', 'foo']), true)
   t.is(any(isFoo, ['foo', 'foo', 'bar']), true)
   t.is(any(isFoo, ['foo', 'bar', 'bar']), true)
   t.is(any(isFoo, ['bar', 'bar', 'bar']), false)
 
-  const isZero = equals(0)
-
   t.is(any(isZero, [0, 1, 0]), true)
   t.is(any(isZero, [1, 0, 1]), true)
   t.is(any(isZero, [1, 1, 1]), false)
+})
+
+test('should test predicate on Map', t => {
+  t.is(any(isFoo, new Map([[1, 'foo'], [2, 'foo'], [3, 'foo']])), true)
+  t.is(any(isFoo, new Map([[1, 'foo'], [2, 'foo'], [3, 'bar']])), true)
+  t.is(any(isFoo, new Map([[1, 'foo'], [2, 'bar'], [3, 'bar']])), true)
+  t.is(any(isFoo, new Map([[1, 'bar'], [2, 'bar'], [3, 'bar']])), false)
+})
+
+test('should test predicate on Object', t => {
+  t.is(any(isFoo, {a: 'foo', b: 'foo', c: 'foo'}), true)
+  t.is(any(isFoo, {a: 'foo', b: 'foo', c: 'bar'}), true)
+  t.is(any(isFoo, {a: 'foo', b: 'bar', c: 'bar'}), true)
+  t.is(any(isFoo, {a: 'bar', b: 'bar', c: 'bar'}), false)
+})
+
+test('should test predicate on Set', t => {
+  t.is(any(isFoo, new Set(['foo', 'foo', 'foo'])), true)
+  t.is(any(isFoo, new Set(['foo', 'foo', 'bar'])), true)
+  t.is(any(isFoo, new Set(['foo', 'bar', 'bar'])), true)
+  t.is(any(isFoo, new Set(['bar', 'bar', 'bar'])), false)
+})
+
+test('should test predicate on String', t => {
+  t.is(any(isA, 'aaa'), true)
+  t.is(any(isA, 'aab'), true)
+  t.is(any(isA, 'abb'), true)
+  t.is(any(isA, 'bbb'), false)
 })
