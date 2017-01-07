d441d23d9f167f808e6bf56d5d4b6b595191fc14
Christian Grøngaard <christianhg@gmail.com>
2017-01-07 11:37:34 +0100

    B.any should provide fallback for non-special cases

--- src/any.js
+++ src/any.js
@@ -1,18 +1,16 @@
 const curry = require('./curry')
 const type = require('./type')
 
-const any = (p, a) =>
-  ({
+const any = (p, a) => {
+  const m = {
     'Array': () => a.some(p),
-    'Boolean': () => false,
     'Map': () => Array.from(a).some(([key, value]) => p(value)),
-    'Null': () => false,
-    'Number': () => false,
     'Object': () => Object.keys(a).some((x) => p(a[x])),
     'Set': () => [...a].some(p),
     'String': () => a.split('').some(c => p(c)),
-    'Symbol': () => false,
-    'Undefined': () => false
-  }[type(a)]())
+    '*': () => false
+  }
+  return (m[type(a)] || m['*'])()
+}
 
 module.exports = curry(any)
