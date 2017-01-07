9f91e45fb131f9e1e58744ed0d71998ec20a459f
Christian Grøngaard <christianhg@gmail.com>
2017-01-07 09:16:39 +0100

    B.equals should compare Dates

--- tests/equals.js
+++ tests/equals.js
@@ -2,16 +2,6 @@ import test from 'ava'
 
 import equals from '../src/equals'
 
-test('should compare Numbers', t => {
-  t.is(equals(1, 1), true)
-  t.is(equals(1, 2), false)
-})
-
-test('should compare Strings', t => {
-  t.is(equals('a', 'a'), true)
-  t.is(equals('a', 'b'), false)
-})
-
 test('should compare Arrays', t => {
   t.is(equals([1, 2, 3], [1, 2, 3]), true)
   t.is(equals([1, 2, 3], [0, 1, 2]), false)
@@ -22,27 +12,19 @@ test('should compare nested Arrays', t => {
   t.is(equals([1, [2, [3]]], [1, [2, [[3]]]]), false)
 })
 
-test('should compare Objects', t => {
-  t.is(equals({a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3}), true)
-  t.is(equals({a: 1, b: 2, c: 3}, {a: 0, b: 1, c: 2}), false)
+test('should compare Dates', t => {
+  t.is(equals(new Date(2016, 2, 19), new Date(2016, 2, 19)), true)
+  t.is(equals(new Date(1900, 9, 12), new Date(1987, 9, 5)), false)
 })
 
-test('should discard order when comparing Objects', t => {
-  t.is(equals({c: 3, b: 2, a: 1}, {a: 1, b: 2, c: 3}), true)
+test('should compare Numbers', t => {
+  t.is(equals(1, 1), true)
+  t.is(equals(1, 2), false)
 })
 
-test('should compare mixed types', t => {
-  t.is(equals({a: 1, b: [2], c: false}, {a: 1, b: [2], c: false}), true)
-})
-
-test('should compare Sets', t => {
-  t.is(equals(new Set([1, 2, 3]), new Set([1, 2, 3])), true)
-  t.is(equals(new Set([1, 2, 3]), new Set([1, 3, 2])), false)
-})
-
-test('should compare Sets containing Objects', t => {
-  t.is(equals(new Set([{a: 1}]), new Set([{a: 1}])), true)
-  t.is(equals(new Set([{a: 1}]), new Set([{b: 1}])), false)
+test('should compare Strings', t => {
+  t.is(equals('a', 'a'), true)
+  t.is(equals('a', 'b'), false)
 })
 
 test('should compare Maps', t => {
@@ -56,4 +38,27 @@ test('should compare Maps', t => {
       new Map([['x', 0], ['y', 1], ['z', 2]]),
       new Map([['x', 1], ['y', 2], ['z', 3]])
     ), false)
+})
+
+test('should compare Objects', t => {
+  t.is(equals({a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3}), true)
+  t.is(equals({a: 1, b: 2, c: 3}, {a: 0, b: 1, c: 2}), false)
+})
+
+test('should discard order when comparing Objects', t => {
+  t.is(equals({c: 3, b: 2, a: 1}, {a: 1, b: 2, c: 3}), true)
+})
+
+test('should compare Sets', t => {
+  t.is(equals(new Set([1, 2, 3]), new Set([1, 2, 3])), true)
+  t.is(equals(new Set([1, 2, 3]), new Set([1, 3, 2])), false)
+})
+
+test('should compare Sets containing Objects', t => {
+  t.is(equals(new Set([{a: 1}]), new Set([{a: 1}])), true)
+  t.is(equals(new Set([{a: 1}]), new Set([{b: 1}])), false)
+})
+
+test('should compare mixed types', t => {
+  t.is(equals({a: 1, b: [2], c: false}, {a: 1, b: [2], c: false}), true)
 })
