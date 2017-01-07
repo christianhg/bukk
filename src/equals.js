22b2f78af287bf5f96754e23daad4143051ac784
Christian Grøngaard <christianhg@gmail.com>
2017-01-07 11:35:34 +0100

    B.equals should provide fallback for non-special cases

--- src/equals.js
+++ src/equals.js
@@ -1,23 +1,20 @@
 const curry = require('./curry')
 const type = require('./type')
 
-const equals = (a, b) =>
-  ({
+const equals = (a, b) => {
+  const m = {
     'Array': () => a.length === b.length &&
       a.every((x, i) => equals(x, b[i])),
-    'Boolean': () => a === b,
     'Date': () => a.getTime() === b.getTime(),
     'Map': () => a.size === b.size &&
       equals(Array.from(a), Array.from(b)),
-    'Null': () => a === b,
-    'Number': () => a === b,
     'Object': () => Object.keys(a).length === Object.keys(b).length &&
       Object.keys(a).every(x => equals(a[x], b[x])),
     'Set': () => a.size === b.size &&
       equals(Array.from(a), Array.from(b)),
-    'String': () => a === b,
-    'Symbol': () => a === b,
-    'Undefined': () => a === b
-  }[type(a)])()
+    '*': () => a === b
+  }
+  return (m[type(a)] || m['*'])()
+}
 
 module.exports = curry(equals)
