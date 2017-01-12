29294a3dfc3df71b4acb582d157f37e7202315c3
Christian Grøngaard <christianhg@gmail.com>
2017-01-12 15:45:54 +0100

    B.head should operate on Arrays, Maps, Objects, Sets and Strings

--- src/head.js
+++ src/head.js
@@ -1,4 +1,16 @@
-const head = xs =>
-  xs[0]
+const type = require('./type')
+
+const head = a => {
+  const m = {
+    'Array': () => a[0],
+    'Map': () => Array.from(a)[0],
+    'Object': () => a[head(Object.keys(a))],
+    'Set': () => [...a][0],
+    'String': () => [...a][0],
+    '*': () => undefined
+  }
+
+  return (m[type(a)] || m['*'])()
+}
 
 module.exports = head
--- tests/head.js
+++ tests/head.js
@@ -4,14 +4,33 @@ import head from '../src/head'
 
 test('should return head of an Array', t => {
   t.is(head([1, 2, 3]), 1)
+  t.is(head(['foo', 'bar', 'baz']), 'foo')
+})
+
+test('should return head of a Map', t => {
+  t.deepEqual(head(new Map([['x', 1], ['y', 2], ['z', 3]])), ['x', 1])
+})
+
+test('should not return the last of a Number', t => {
+  t.is(head(42), undefined)
+})
+
+test('should return the head of an Object', t => {
+  t.is(head({x: 1, y: 2, z: 3}), 1)
+})
+
+test('should return head of a Set', t => {
+  t.is(head(new Set([1, 2, 3])), 1)
+  t.is(head(new Set(['foo', 'bar', 'baz'])), 'foo')
 })
 
 test('should return head of a String', t => {
   t.is(head('foo'), 'f')
+  t.is(head('bar'), 'b')
+  t.is(head('baz'), 'b')
 })
 
 test('should return undefined if head does not exist', t => {
   t.is(head([]), undefined)
   t.is(head(''), undefined)
-  t.is(head({x: 1, y: 2, z: 3}), undefined)
 })
