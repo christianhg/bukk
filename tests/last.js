95b1f2d638c5c76535fc89e0babd8b860031f0ef
Christian Grøngaard <christianhg@gmail.com>
2017-02-02 19:14:48 +0100

    Fix typos in test of B.last

--- tests/last.js
+++ tests/last.js
@@ -2,29 +2,29 @@ import test from 'ava'
 
 import last from '../src/last'
 
-test('should return last of an Array', t => {
+test('should operate on an Array', t => {
   t.is(last([1, 2, 3]), 3)
   t.is(last(['foo', 'bar', 'baz']), 'baz')
 })
 
-test('should return last of a Map', t => {
+test('should operate on a Map', t => {
   t.deepEqual(last(new Map([['x', 1], ['y', 2], ['z', 3]])), ['z', 3])
 })
 
-test('should not return the last of a Number', t => {
+test('should not operate on a Number', t => {
   t.is(last(42), undefined)
 })
 
-test('should return the head of an Object', t => {
+test('should operate on an Object', t => {
   t.is(last({x: 1, y: 2, z: 3}), 3)
 })
 
-test('should return last of a Set', t => {
+test('should operate on a Set', t => {
   t.is(last(new Set([1, 2, 3])), 3)
   t.is(last(new Set(['foo', 'bar', 'baz'])), 'baz')
 })
 
-test('should return last of a String', t => {
+test('should operate on a String', t => {
   t.is(last('foo'), 'o')
   t.is(last('bar'), 'r')
   t.is(last('baz'), 'z')
