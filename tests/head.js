6525803fa8b964b937293ff2b0b5e2e3c87032d2
Christian Grøngaard <christianhg@gmail.com>
2017-02-02 19:15:30 +0100

    Fix typos in test of B.head

--- tests/head.js
+++ tests/head.js
@@ -2,29 +2,29 @@ import test from 'ava'
 
 import head from '../src/head'
 
-test('should return head of an Array', t => {
+test('should operate on an Array', t => {
   t.is(head([1, 2, 3]), 1)
   t.is(head(['foo', 'bar', 'baz']), 'foo')
 })
 
-test('should return head of a Map', t => {
+test('should operate on a Map', t => {
   t.deepEqual(head(new Map([['x', 1], ['y', 2], ['z', 3]])), ['x', 1])
 })
 
-test('should not return the last of a Number', t => {
+test('should not operate on a Number', t => {
   t.is(head(42), undefined)
 })
 
-test('should return the head of an Object', t => {
+test('should operate on an Object', t => {
   t.is(head({x: 1, y: 2, z: 3}), 1)
 })
 
-test('should return head of a Set', t => {
+test('should operate on a Set', t => {
   t.is(head(new Set([1, 2, 3])), 1)
   t.is(head(new Set(['foo', 'bar', 'baz'])), 'foo')
 })
 
-test('should return head of a String', t => {
+test('should operate on a String', t => {
   t.is(head('foo'), 'f')
   t.is(head('bar'), 'b')
   t.is(head('baz'), 'b')
