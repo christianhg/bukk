cfcdf08c8ca9fa4a51484da5f50b0115cbf92be3
Christian Grøngaard <christianhg@gmail.com>
2017-02-02 19:14:35 +0100

    Fix typos in test of B.take

--- tests/take.js
+++ tests/take.js
@@ -2,7 +2,7 @@ import test from 'ava'
 
 import take from '../src/take'
 
-test('should take from an Array', t => {
+test('should operate on an Array', t => {
   t.deepEqual(take(3, [1, 2, 3, 4, 5]), [1, 2, 3])
   t.deepEqual(take(1, [1, 2, 3, 4, 5]), [1])
 })
@@ -11,11 +11,11 @@ test('should operate on a Map', t => {
   t.deepEqual(take(2, new Map([['a', 1], ['b', 2], ['c', 3]])), new Map([['a', 1], ['b', 2]]))
 })
 
-test('should not take from a Number', t => {
+test('should not operate on a Number', t => {
   t.is(take(1, 42), 42)
 })
 
-test('should not take from an Object', t => {
+test('should not operate on an Object', t => {
   t.deepEqual(take(1, {a: 1, b: 2}), {a: 1, b: 2})
 })
 
@@ -23,7 +23,7 @@ test('should operate on a Set', t => {
   t.deepEqual(take(3, new Set(['a', 'b', 'c', 'd', 'e'])), new Set(['a', 'b', 'c']))
 })
 
-test('should slice a String', t => {
+test('should operate on a String', t => {
   t.is(take(3, 'foobar'), 'foo')
   t.is(take(5, 'hello world'), 'hello')
 })
