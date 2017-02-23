e48afea4da2e34885ee75bbb5dbff496cfd01028
Christian Grøngaard <christianhg@gmail.com>
2017-02-23 18:32:16 +0100

    Avoid unneeded recursion in B.any

--- src/any.js
+++ src/any.js
@@ -7,8 +7,8 @@ const any = (p, a) => {
     'Boolean': () => p(a),
     'Map': () => [...a].some(([key, value]) => p(value)),
     'Object': () => Object.keys(a).some((x) => p(a[x])),
-    'Set': () => any(p, [...a]),
-    'String': () => any(p, [...a]),
+    'Set': () => [...a].some(p),
+    'String': () => [...a].some(p),
     '*': () => false
   }
   return (m[kind(a)] || m['*'])()
