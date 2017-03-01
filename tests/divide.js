7b6a7944bc94178dee65b688fd02db8ec6d74dda
Christian Grøngaard <christianhg@gmail.com>
2017-03-01 19:03:25 +0100

    Improve tests for B.divide

--- tests/divide.js
+++ tests/divide.js
@@ -2,8 +2,17 @@ import test from 'ava'
 
 import divide from '../src/divide'
 
-test('should divide Numbers', t => {
+test('should operate on Numbers', t => {
   t.is(divide(1, 2), 0.5)
   t.is(divide(25, 100), 0.25)
   t.is(divide(42, 3.5), 12)
+})
+
+test('should not operate on Strings', t => {
+  t.true(Number.isNaN(divide('a', 'b')))
+})
+
+test('should not operate on mixed kinds', t => {
+  t.true(Number.isNaN(divide('a', 1)))
+  t.true(Number.isNaN(divide(0, 'b')))
 })
