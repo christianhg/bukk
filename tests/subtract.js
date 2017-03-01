b5c8e140979483606c7df281361047d59ca69ba3
Christian Grøngaard <christianhg@gmail.com>
2017-03-01 19:04:04 +0100

    Improve tests for B.subtract

--- tests/subtract.js
+++ tests/subtract.js
@@ -2,9 +2,18 @@ import test from 'ava'
 
 import subtract from '../src/subtract'
 
-test('should subtract Numbers', t => {
+test('should operate on Numbers', t => {
   t.is(subtract(1, 1), 0)
   t.is(subtract(-1, 1), -2)
   t.is(subtract(-1, -1), 0)
   t.is(subtract(1, -1), 2)
+})
+
+test('should not operate on Strings', t => {
+  t.true(Number.isNaN(subtract('a', 'b')))
+})
+
+test('should not operate on mixed kinds', t => {
+  t.true(Number.isNaN(subtract('a', 1)))
+  t.true(Number.isNaN(subtract(0, 'b')))
 })
