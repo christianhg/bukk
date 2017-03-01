40782196007a665f30687d8d721cc3b560b1f36b
Christian Grøngaard <christianhg@gmail.com>
2017-03-01 19:03:14 +0100

    Improve tests for B.dec

--- tests/dec.js
+++ tests/dec.js
@@ -2,9 +2,13 @@ import test from 'ava'
 
 import dec from '../src/dec'
 
-test('should decrement a Number', t => {
+test('should operate on a Number', t => {
   t.is(dec(0), -1)
   t.is(dec(1), 0)
   t.is(dec(-1), -2)
   t.is(dec(42), 41)
+})
+
+test('should not operate on a String', t => {
+  t.true(Number.isNaN(dec('a')))
 })
