d7deef43722c170d4200276f543c0db3927f7528
Christian Grøngaard <christianhg@gmail.com>
2017-03-02 19:41:24 +0100

    B.min should only operate on Numbers

--- src/min.js
+++ src/min.js
@@ -1,8 +1,15 @@
 import curry from './curry'
+import kind from './kind'
 
-const min = (a, b) =>
-  b < a
-    ? b
-    : a
+const min = (a, b) => {
+  const m = {
+    'Number': () => b < a
+      ? b
+      : a,
+    '*': () => undefined
+  }
+
+  return ((kind(a) === kind(b) && m[kind(a)]) || m['*'])()
+}
 
 export default curry(min)
--- tests/min.js
+++ tests/min.js
@@ -2,22 +2,12 @@ import test from 'ava'
 
 import min from '../src/min'
 
-test('should operate on Arrays', t => {
-  t.deepEqual(min([1], [2]), [1])
-  t.deepEqual(min([2], [1]), [1])
-  t.deepEqual(min([1, 200, 3], [2]), [1, 200, 3])
-  t.deepEqual(min([1, 2, 3, 4], [2, 3, 4]), [1, 2, 3, 4])
-  t.deepEqual(min([1, 200, 3, 4], [0, 1, 2, 3]), [0, 1, 2, 3])
-})
-
 test('should operate on Numbers', t => {
   t.is(min(1, 2), 1)
   t.is(min(2, 2), 2)
   t.is(min(3, 2), 2)
 })
 
-test('should operate on Strings', t => {
-  t.is(min('a', 'b'), 'a')
-  t.is(min('c', 'b'), 'b')
-  t.is(min('c', 'd'), 'c')
+test('should not operate on Strings', t => {
+  t.is(min('a', 'b'), undefined)
 })
