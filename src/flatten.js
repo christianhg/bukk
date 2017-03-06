146d4c9fbf3086f60f6198f5dfc96812bffb9022
Christian Grøngaard <christianhg@gmail.com>
2017-03-06 19:54:29 +0100

    B.flatten should only operate on Arrays

--- src/flatten.js
+++ src/flatten.js
@@ -1,9 +1,15 @@
 import kind from './kind'
 
-const flatten = a =>
-  a.reduce((b, x) =>
-    kind(x) === 'Array'
-      ? [...b, ...flatten(x)]
-      : [...b, x], [])
+const flatten = a => {
+  const m = {
+    'Array': () => a.reduce((b, x) =>
+      kind(x) === 'Array'
+        ? [...b, ...flatten(x)]
+        : [...b, x], []),
+    '*': () => a
+  }
+
+  return (m[kind(a)] || m['*'])()
+}
 
 export default flatten
--- tests/flatten.js
+++ tests/flatten.js
@@ -2,10 +2,14 @@ import test from 'ava'
 
 import flatten from '../src/flatten'
 
-test('should flatten an Array', t => {
+test('should operate on an Array', t => {
   t.deepEqual(flatten([1, [2, [3]]]), [1, 2, 3])
   t.deepEqual(
     flatten([1, 2, [3], 4, [5, 6, [7, 8, [9]], 10]]),
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   )
+})
+
+test('should not operate on a String', t => {
+  t.deepEqual(flatten('foo'), 'foo')
 })
