7366d78072e32e29e929fbcec8a4378c8bccb2c2
Christian Grøngaard <christianhg@gmail.com>
2017-03-06 20:11:07 +0100

    B.gte should only operate on Numbers

--- src/gte.js
+++ src/gte.js
@@ -1,6 +1,13 @@
 import curry from './curry'
+import kind from './kind'
 
-const gte = (a, b) =>
-  a >= b
+const gte = (a, b) => {
+  const m = {
+    'Number': () => a >= b,
+    '*': () => undefined
+  }
+
+  return ((kind(a) === kind(b) && m[kind(a)]) || m['*'])()
+}
 
 export default curry(gte)
--- tests/gte.js
+++ tests/gte.js
@@ -2,8 +2,12 @@ import test from 'ava'
 
 import gte from '../src/gte'
 
-test('should compare Numbers', t => {
+test('should operate on Numbers', t => {
   t.is(gte(1, 2), false)
   t.is(gte(1, 1), true)
   t.is(gte(1, 0), true)
+})
+
+test('should not operate on Strings', t => {
+  t.is(gte('a', 'b'), undefined)
 })
