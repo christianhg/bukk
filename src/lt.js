40ac493d5d35f9e057903d258175b0a2dfefdc93
Christian Grøngaard <christianhg@gmail.com>
2017-03-06 20:11:25 +0100

    B.lt should only operate on Numbers

--- src/lt.js
+++ src/lt.js
@@ -1,6 +1,13 @@
 import curry from './curry'
+import kind from './kind'
 
-const lt = (a, b) =>
-  a < b
+const lt = (a, b) => {
+  const m = {
+    'Number': () => a < b,
+    '*': () => undefined
+  }
+
+  return ((kind(a) === kind(b) && m[kind(a)]) || m['*'])()
+}
 
 export default curry(lt)
--- tests/lt.js
+++ tests/lt.js
@@ -2,8 +2,12 @@ import test from 'ava'
 
 import lt from '../src/lt'
 
-test('should compare Numbers', t => {
+test('should operate Numbers', t => {
   t.is(lt(1, 2), true)
   t.is(lt(1, 1), false)
   t.is(lt(1, 0), false)
+})
+
+test('should not operate on Strings', t => {
+  t.is(lt('a', 'b'), undefined)
 })
