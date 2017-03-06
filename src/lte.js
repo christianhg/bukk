a618c9ecad3d503ce57e768a1924b4a053b6c97d
Christian Grøngaard <christianhg@gmail.com>
2017-03-06 20:11:17 +0100

    B.lte should only operate on Numbers

--- src/lte.js
+++ src/lte.js
@@ -1,6 +1,13 @@
 import curry from './curry'
+import kind from './kind'
 
-const lte = (a, b) =>
-  a <= b
+const lte = (a, b) => {
+  const m = {
+    'Number': () => a <= b,
+    '*': () => undefined
+  }
+
+  return ((kind(a) === kind(b) && m[kind(a)]) || m['*'])()
+}
 
 export default curry(lte)
--- tests/lte.js
+++ tests/lte.js
@@ -2,8 +2,12 @@ import test from 'ava'
 
 import lte from '../src/lte'
 
-test('should compare Numbers', t => {
+test('should operate on Numbers', t => {
   t.is(lte(1, 2), true)
   t.is(lte(1, 1), true)
   t.is(lte(1, 0), false)
+})
+
+test('should not operate on Strings', t => {
+  t.is(lte('a', 'b'), undefined)
 })
