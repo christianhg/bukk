755f4c95ab4c3f3de2b406415102a337a737536f
Christian Grøngaard <christianhg@gmail.com>
2017-03-06 20:10:55 +0100

    B.gt should only operate on Numbers

--- src/gt.js
+++ src/gt.js
@@ -1,6 +1,13 @@
 import curry from './curry'
+import kind from './kind'
 
-const gt = (a, b) =>
-  a > b
+const gt = (a, b) => {
+  const m = {
+    'Number': () => a > b,
+    '*': () => undefined
+  }
+
+  return ((kind(a) === kind(b) && m[kind(a)]) || m['*'])()
+}
 
 export default curry(gt)
--- tests/gt.js
+++ tests/gt.js
@@ -2,8 +2,12 @@ import test from 'ava'
 
 import gt from '../src/gt'
 
-test('should compare Numbers', t => {
+test('should operate on Numbers', t => {
   t.is(gt(1, 2), false)
   t.is(gt(1, 1), false)
   t.is(gt(1, 0), true)
+})
+
+test('should not operate on Strings', t => {
+  t.is(gt('a', 'b'), undefined)
 })
